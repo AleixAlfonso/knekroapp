f9cebce4090678b63190d751590e8bcd8fc1ac6b
AleixAlfonso <aleixalfonsoilerna@gmail.com>
2021-05-30 19:57:46 +0200

    version 2.

--- lib/controllers/bd_controller.dart
+++ lib/controllers/bd_controller.dart
@@ -37,7 +37,6 @@ class FirebaseApi {
       'icono': icono,
       'rutaAudio': rutaAudio,
       'titulo': titulo,
-      'fecha': date.toString()
     });
   }
 }
--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -12,7 +12,8 @@ class InfoPage extends StatelessWidget {
       body: Container(
         child: Padding(
           padding: EdgeInsets.all(15),
-          child: GridView.count(             crossAxisCount: 2,
+          child: GridView.count(
+            crossAxisCount: 1,
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
             children: [
@@ -42,21 +43,22 @@ class InfoPage extends StatelessWidget {
                     children: [
                       Text(
                         'Streamer',
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white, fontSize: 30),
                       ),
                       SizedBox(
-                        height: 10,
+                        height: 20,
                       ),
                       CircleAvatar(
-                        radius: 30,
+                        radius: 60,
                         backgroundImage: AssetImage('assets/Miscelanea.png'),
                       ),
                       SizedBox(
-                        height: 10,
+                        height: 30,
                       ),
-                      Text('Knekro', style: TextStyle(color: Colors.white)),
+                      Text('Knekro',
+                          style: TextStyle(color: Colors.white, fontSize: 30)),
                       SizedBox(
-                        height: 10,
+                        height: 30,
                       ),
                       socialMedia(
                           'https://twitter.com/KNekro',
@@ -69,30 +71,47 @@ class InfoPage extends StatelessWidget {
               ClipRRect(
                 borderRadius: BorderRadius.circular(20),
                 child: Container(
-                  color: Colors.grey,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color(0XFF8789C0),
+                          Color(0XFF45F0DF),
+                          Color(0XFFC2CAE8),
+                          Color(0XFF8380B6),
+                          Color(0XFF111D4A),
+                        ],
+                        stops: [
+                          0.20,
+                          0.40,
+                          0.60,
+                          0.80,
+                          1
+                        ]),
+                  ),
                   child: Column(
                     children: [
                       Text(
-                        'Streamer',
-                        style: TextStyle(color: Colors.white),
+                        'App Creator',
+                        style: TextStyle(color: Colors.white, fontSize: 30),
                       ),
                       SizedBox(
-                        height: 10,
+                        height: 20,
                       ),
                       CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('assets/Miscelanea.png'),
+                        radius: 60,
+                        backgroundImage: AssetImage('assets/aleix.png'),
                       ),
                       SizedBox(
-                        height: 10,
+                        height: 30,
                       ),
-                      Text('Knekro', style: TextStyle(color: Colors.white)),
+                      Text('Aleixmen',
+                          style: TextStyle(color: Colors.white, fontSize: 30)),
                       SizedBox(
-                        height: 10,
+                        height: 30,
                       ),
-                      socialMedia(
-                          'https://twitter.com/KNekro',
-                          'https://www.youtube.com/user/KNekroGamer',
+                      socialMediaAleix('https://twitter.com/AleixAlfonso',
                           'https://www.twitch.tv/knekro'),
                     ],
                   ),
@@ -128,6 +147,8 @@ class InfoPage extends StatelessWidget {
               //     ),
               //   ),
               // )
+              Text(
+                  'Reporte de bugs y sugerencias en el twitter del App Creator.')
             ],
           ),
         ),
@@ -137,11 +158,13 @@ class InfoPage extends StatelessWidget {
 
   socialMedia(String urltwitter, String urlyoutube, String urltwitch) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
           width: 10,
         ),
         IconButton(
+            iconSize: 60,
             onPressed: () async {
               await canLaunch(urltwitter)
                   ? await launch(urltwitter)
@@ -149,7 +172,7 @@ class InfoPage extends StatelessWidget {
             },
             icon: Image.asset('assets/TwitterLogo.png')),
         SizedBox(
-          width: 5,
+          width: 40,
         ),
         IconButton(
           onPressed: () async {
@@ -158,12 +181,13 @@ class InfoPage extends StatelessWidget {
                 : throw 'Could not launch https://www.twitch.tv/knekro';
           },
           icon: Image.asset('assets/TwitchLogo.png'),
-          iconSize: 30,
+          iconSize: 60,
         ),
         SizedBox(
-          width: 5,
+          width: 40,
         ),
         IconButton(
+            iconSize: 60,
             onPressed: () async {
               await canLaunch('https://www.youtube.com/user/KNekroGamer')
                   ? await launch('https://www.youtube.com/user/KNekroGamer')
@@ -176,32 +200,29 @@ class InfoPage extends StatelessWidget {
 
   socialMediaAleix(String urltwitter, String urltwitch) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
           width: 10,
         ),
         IconButton(
+            iconSize: 60,
             onPressed: () async {
               await canLaunch(urltwitter)
                   ? await launch(urltwitter)
                   : throw 'Could not launch $urltwitter';
             },
             icon: Image.asset('assets/TwitterLogo.png')),
-        SizedBox(
-          width: 5,
-        ),
-        IconButton(
-          onPressed: () async {
-            await canLaunch('https://www.twitch.tv/knekro')
-                ? await launch('https://www.twitch.tv/knekro')
-                : throw 'Could not launch https://www.twitch.tv/knekro';
-          },
-          icon: Image.asset('assets/TwitchLogo.png'),
-          iconSize: 30,
-        ),
-        SizedBox(
-          width: 5,
-        ),
+
+        // IconButton(
+        //   onPressed: () async {
+        //     await canLaunch('https://www.twitch.tv/knekro')
+        //         ? await launch('https://www.twitch.tv/knekro')
+        //         : throw 'Could not launch https://www.twitch.tv/knekro';
+        //   },
+        //   icon: Image.asset('assets/TwitchLogo.png'),
+        //   iconSize: 30,
+        // ),
       ],
     );
   }
